66b477be6ce6743c8946e24f08d0c7dd3d0c5efb
Thender2022 <Thender2022@aol.com>
2023-03-08 12:05:44 -0800

    image update

---
+++ src/data.js
@@ -0,0 +1,66 @@
+const data = {
+    products: [
+        {
+            name: 'Abstract',
+            slug: 'abstract',
+            category: 'Paintings',
+            image: '/images/abstract.jpg',
+            price: 500,
+            countInStock: 1,
+            brand: 'Lurker',
+            description: 'High quality art'
+        },
+        {
+            name: 'Hurt',
+            slug: 'hurt',
+            category: 'Paintings',
+            image: '/images/Hurt.jpg',
+            price: 500,
+            countInStock: 1,
+            brand: 'Lurker',
+            description: 'High quality art'
+        },
+        {
+            name: 'Purp',
+            slug: 'purp',
+            category: 'Paintings',
+            image: '/images/purp.jpg',
+            price: 500,
+            countInStock: 1,
+            brand: 'Lurker',
+            description: 'High quality art'
+        },
+        {
+            name: 'Peyote',
+            slug: 'peyote',
+            category: 'Paintings',
+            image: '/images/peyote.jpg',
+            price: 500,
+            countInStock: 1,
+            brand: 'Lurker',
+            description: 'High quality art'
+        },
+        {
+            name: 'Saved',
+            slug: 'saved',
+            category: 'Paintings',
+            image: '/images/saved.jpg',
+            price: 500,
+            countInStock: 1,
+            brand: 'Lurker',
+            description: 'High quality art'
+        },
+        {
+            name: 'Smithers',
+            slug: 'smithers',
+            category: 'Paintings',
+            image: '/images/smithers.jpg',
+            price: 500,
+            countInStock: 1,
+            brand: 'Lurker',
+            description: 'High quality art'
+        },
+    ]
+}
+
+export default data;
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -6,25 +6,43 @@ import AuthPage from '../AuthPage/AuthPage';
 import NewOrderPage from '../NewOrderPage/NewOrderPage';
 import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
 import NavBar from '../../components/NavBar/NavBar'
+import data from '../../data';
+
+
 
 export default function App() {
   const [ user, setUser ] = useState(getUser())
 
   return (
-    <main className="App">
+    <div className="App">
       {
         user ?
         <>
+        <header>
+          <a href="/">Lurker</a>
+        </header>
+        <main>
           <NavBar user={user} setUser={setUser} />
-          <Routes>
+          <h1>Art Work</h1>
+          {
+            data.products.map(product => (
+            <div>
+              <img src={product.image} alt={product.name} />
+              <p></p>
+              <p></p>
+            </div>
+            ))
+          }
+          {/* <Routes>
             <Route path="/orders/new" element={<NewOrderPage />} />
             <Route path="/orders" element={<OrderHistoryPage />} />
-          </Routes>
+          </Routes> */}
+        </main>
         </>
         :
         <AuthPage setUser={setUser} />
       }
-    </main>
+    </div>
   );
 }
 
