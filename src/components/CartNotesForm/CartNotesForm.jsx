16d848292cd04525677f0de5cc706b7d6421411d
Thender2022 <Thender2022@aol.com>
2023-03-16 11:07:41 -0700

    handle change complete

--- src/components/CartNotesForm/CartNotesForm.jsx
+++ src/components/CartNotesForm/CartNotesForm.jsx
@@ -1,10 +1,30 @@
+import { useState } from "react"
+
 export default function CartNotesForm() {
+    const [newNote, setNewNote] = useState({
+        content: '',
+        budget: 0
+    })
+
+    function handleChange(evt) {
+        const newNoteForm = {
+            ...newNote,
+            [evt.target.name]: evt.target.value
+        }
+        setNewNote(newNoteForm)
+    }
+
+    function handleSubmit(evt) {
+        evt.preventDefault();
+        
+    }
+
     return (
         <>
             <div>CartNotesForm</div>
-            <form>
-                <input type="text" name="content" placeholder="Note..."></input>
-                <input type="number" name="budget" placeholder="$$$"></input>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="content" onChange={handleChange} value={newNote.content} placeholder="Note..."></input>
+                <input type="number" name="budget" onChange={handleChange} value={newNote.budget} placeholder="$$$"></input>
                 <button type="submit">Submit</button>
             </form>
         </>
