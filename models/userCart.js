3ac4276c74debeaf72eaa22fb2f645bef3137fd6
Thender2022 <Thender2022@aol.com>
2023-03-16 10:19:50 -0700

    cart update

--- models/userCart.js
+++ models/userCart.js
@@ -1,13 +1,32 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const cartNotesSchema = new Schema({
+  content: {
+    type: String,
+    required: true
+  }, 
+  budget: {
+    type: Number,
+    min: 0,
+    max: 2000,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }}, {
+    timestamps: true
+});
 
 const userCartSchema = new Schema({
   artPieces: { type: Array, default: [] },
-  user: { type: Schema.Types.ObjectId, ref: "User", required: true }
+  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  cartNotesSchema: [cartNotesSchema]
 }, {
   timestamps: true,
 });
+
 
 
 
