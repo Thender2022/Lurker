b0c7eda7109bf7dcb412f53501c813f4e212f1a8
Thender2022 <Thender2022@aol.com>
2023-03-19 17:58:23 -0700

    css update

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,17 @@
+import './HomePage.css';
+
 export default function HomePage() {
     return (
-        <div>
+        <div className="home">
           <h1>Who is Eric White?</h1>
-          <img className="fro" src="images/Specs.jpg" />
-          <p>From the East Coast to the West Eric is known for is great spirit and creative energy. Solely living life through his passion and bringing people of all backgrounds together through his art! While he is not creating amazing pieces on canvas he is out in the field painting murals, modeling for your favorite brands or you may catch him outside cruising through Venice Beach. With all that being said welcome to your new favorite humans website!</p>
+          <div className="intro">
+            <img className="fro" src="images/Specs.jpg" />
+            <div class="text">
+                <p>From the East Coast to the West Eric is known for his great spirit and creative energy. Solely living life through his passion and bringing people of all backgrounds together through his art! <br></br><br></br>While he is not creating amazing pieces on canvas he is out in the field painting murals, modeling for your favorite brands or you may catch him outside cruising through Venice Beach. With all that being said welcome to your new favorite artists website!</p>
+                <img className="image2" src="images/Cheese.jpg"/>
+            </div>
+          </div>
+            <img className="vacation" src="images/ForeverVacation.jpg" />
         </div>
     )
 }
